4a66e71e72c8d63e4935db6be3ec57a552b5fa20
PoornimaKarma <161825445+PoornimaKarma@users.noreply.github.com>
2024-06-10 12:58:39 +0530

    Add files via upload

---
+++ Arithmaticexcp.java
@@ -0,0 +1,18 @@
+package Exception;
+
+public class Arithmaticexcp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int a=100/0;
+		}
+		catch (Exception e) {
+		
+		  
+			System.out.println(e);
+		}
+		System.out.println("arithmatic axception");
+	}
+
+}
